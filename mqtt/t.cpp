4972394010fc574fa3835d7bbc5f536680089181
Hoàng Ngọc Dung <dunghoang0401237@gmail.com>
2025-10-24 19:05:12 +0700

    update

--- mqtt/t.cpp
+++ mqtt/t.cpp
@@ -1,24 +1,14 @@
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("[MQTT] Attempting connection...");
-    
-    String clientId = "AquaNova_ESP32_C3_";
-    clientId += WiFi.macAddress();
-    clientId.replace(":", "");
-    
-    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
-      Serial.println(" connected!");
-      if(client.subscribe(subscribe_topic)) {
-          Serial.print("[MQTT] Subscribed to: ");
-          Serial.println(subscribe_topic);
-      } else {
-          Serial.println("[ERROR] Subscribe failed!");
-      }
-    } else {
-      Serial.print(" failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" - trying again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
+@dashboard_bp.get("/summary")
+def summary():
+    db = firestore.client()
+    ponds = db.collection("readings").stream()
+    devices = db.collection("feed_logs").stream()
+
+    pond_count = len(list(ponds))
+    device_count = len(list(devices))
+
+    return jsonify({
+        "ponds": pond_count,
+        "devices": device_count
+    })
+
