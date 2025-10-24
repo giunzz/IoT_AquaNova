8512af415727605c61e7fcb0a6f0f9c20a17161b
Hoàng Ngọc Dung <dunghoang0401237@gmail.com>
2025-10-24 11:43:49 +0700

    update final code

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev")
-
-    # Firebase
-    FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
-    FIREBASE_DB = os.getenv("FIREBASE_DB", "firestore")
-
-    # MQTT
-    MQTT_HOST = os.getenv("MQTT_HOST", "localhost")
-    MQTT_PORT = int(os.getenv("MQTT_PORT", "1883"))
-    MQTT_USER = os.getenv("MQTT_USER", "")          # optional
-    MQTT_PASS = os.getenv("MQTT_PASS", "")          # optional
-    MQTT_TOPIC = os.getenv("MQTT_TOPIC", "aquanova/devices/+/telemetry")
+def create_app():
+    app = Flask(__name__, static_folder="static", template_folder="templates")
+    app.config.from_object(Config)
+    CORS(app)
--- mqtt/t.cpp
+++ mqtt/t.cpp
@@ -1,15 +1,24 @@
-{'cmd': 'feed', 'amount': 10}
-127.0.0.1 - - [16/Oct/2025 12:00:11] "POST /control/feed-now HTTP/1.1" 200 -
-{'cmd': 'feed', 'amount': 10}
-127.0.0.1 - - [16/Oct/2025 12:00:11] "POST /control/feed-now HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:12] "POST /control/feed-now HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:12] "POST /control/feed-now HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:12] "POST /control/feed-now HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:12] "POST /control/feed-now HTTP/1.1" 200 -
-2025-10-16T12:00:33.326305 [MQTT] 📩 MESSAGE topic=aquanova/devices/telemetry payload={'turbidity':
-2025-10-16T12:00:33.346791 [MQTT] 📩 MESSAGE topic=aquanova/devices/telemetry payload={'turbidity':
-25-10-16T05:00:33.013879+00:00'}
-127.0.0.1 - - [16/Oct/2025 12:00:35] "GET / HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:36] "GET /static/css/styles.css HTTP/1.1" 304 -
-127.0.0.1 - - [16/Oct/2025 12:00:36] "GET /dashboard/last HTTP/1.1" 200 -
-127.0.0.1 - - [16/Oct/2025 12:00:36] "GET /static/img/logo_aquanova.png HTTP/1.1" 304 -
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("[MQTT] Attempting connection...");
+    
+    String clientId = "AquaNova_ESP32_C3_";
+    clientId += WiFi.macAddress();
+    clientId.replace(":", "");
+    
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println(" connected!");
+      if(client.subscribe(subscribe_topic)) {
+          Serial.print("[MQTT] Subscribed to: ");
+          Serial.println(subscribe_topic);
+      } else {
+          Serial.println("[ERROR] Subscribe failed!");
+      }
+    } else {
+      Serial.print(" failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" - trying again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
